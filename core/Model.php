02975390dfdd93ebaafe487ad8cac7a7b8db0bf2
QangHy <150018025+QangHy@users.noreply.github.com>
2025-07-16 21:10:14 +0700

    core

---
+++ core/Model.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'core/Database.php';
+
+class Model  {
+    protected $db;
+    protected $table;
+
+        //khởi tạo với kết nối CSDL
+    public function __construct($table) {
+        $this->db = new Database();
+        $this->table = $table;
+    }
+    //lấy tất cả bảng ghi
+    public function all(){
+        return $this->db->query("SELECT * FROM {$this->table}");
+    }
+    //lấy bảng ghi theo id
+    public function find($id){
+        $return = $this-> db->query("SELECT * FROM {$this->table} WHERE id = ?", [$id]);
+        return $reutrn ? $return[0] : null;
+    }
+}
+?>
